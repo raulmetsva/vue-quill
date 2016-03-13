c026cdd60996e3a62cb5efedf1a8bf378744c8f7
Brock Reece <Brock Reece>
2016-03-13 18:19:25 +0000

    Basic quill component

---
+++ src/Quill.vue
@@ -0,0 +1,19 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                editor: {},
+            }
+        },
+
+        ready() {
+            this.editor = new Quill(this.$el, {
+                theme: 'snow',
+            })
+        }
+    }
+</script>
