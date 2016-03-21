ab4928d1cd24e23c930dad84c21896ed3421ad7c
Brock Reece <brockreece@gmail.com>
2016-03-21 13:07:28 +0000

    Add new custom filter for previewingtext on a quill delta

--- vue-quill.js
+++ vue-quill.js
@@ -12,5 +12,17 @@ module.exports = {
         Vue.filter('quill', function(value) {
             return render(value.ops);
         });
+
+        Vue.filter('quill-preview', function(value, limit) {
+            const text = value.ops.map(function(op) {
+                return op.insert
+            }).join(' ')
+
+            if (typeof limit !== 'undefined' && text.length > limit) {
+                return `${text.substring(0, parseInt(limit, 10))}...`
+            }
+
+            return text
+        });
     },
 }
