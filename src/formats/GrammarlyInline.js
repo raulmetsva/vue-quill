c1707bb30f6555bb0aba579c96ad322a9c29c0a6
Paul Watkins <paul.watkins@croud.com>
2017-03-27 13:27:01 +0100

    Revert "Temporary fix for issue using uglifyjs with js classes"
    This reverts commit eab5f04c8ce71091232b5aee30626e65850f79c2.

--- src/formats/GrammarlyInline.js
+++ src/formats/GrammarlyInline.js
@@ -1,50 +1,10 @@
+const Quill = require('quill')
 
-/*
- *
- * There is an with uglify transpiling classes, so using babel converted version
- *
- * see: http://stackoverflow.com/questions/36482076/how-to-uglify-javascript-classes
- *
- */
+const Inline = Quill.import('blots/inline')
 
-'use strict';
+class GrammarlyInline extends Inline {}
+GrammarlyInline.tagName = 'G'
+GrammarlyInline.blotName = 'grammarly-inline'
+GrammarlyInline.className = 'gr_'
 
-const Quill = require('quill');
-const Inline = Quill.import('blots/inline.js');
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-var GrammarlyInline = function (_Inline) {
-  _inherits(GrammarlyInline, _Inline);
-
-  function GrammarlyInline() {
-    _classCallCheck(this, GrammarlyInline);
-
-    return _possibleConstructorReturn(this, (GrammarlyInline.__proto__ || Object.getPrototypeOf(GrammarlyInline)).apply(this, arguments));
-  }
-
-  return GrammarlyInline;
-}(Inline);
-
-;
-GrammarlyInline.tagName = 'G';
-GrammarlyInline.blotName = 'grammarly-inline';
-GrammarlyInline.className = 'gr_';
-
-module.exports = GrammarlyInline;
-
-
-// const Quill = require('quill')
-
-// const Inline = Quill.import('blots/inline')
-
-// class GrammarlyInline extends Inline {}
-// GrammarlyInline.tagName = 'G'
-// GrammarlyInline.blotName = 'grammarly-inline'
-// GrammarlyInline.className = 'gr_'
-
-// module.exports = GrammarlyInline
+module.exports = GrammarlyInline
